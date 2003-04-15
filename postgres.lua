e4701b993591e2245c0e68dce385d78641f38702
tomas <tomas>
2003-04-15 17:05:40 +0000

    Corrigindo a extensao .dylib (Mac OS X) para .so (Linux).

--- postgres.lua
+++ postgres.lua
@@ -1,5 +1,5 @@
 if not luasql and loadlib then
-	local libname = "libluasqlpostgres.2.0.dylib"
+	local libname = "libluasqlpostgres.2.0.so"
 	local libopen = "luasql_libopen_postgres"
 	local init, err1, err2 = loadlib (libname, libopen)
 	assert (init, (err1 or '')..(err2 or ''))
