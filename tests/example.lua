b7cd32bc90fb1016316118d66791d11e8802ea26
tomas <tomas>
2003-05-02 15:16:15 +0000

    Correcoes de estilo. Troca de nomes no exemplo por nomes ficticios.

--- tests/example.lua
+++ tests/example.lua
@@ -1,41 +1,36 @@
 -- load driver
 require"postgres"
 -- create environment object
-env, err = luasql.postgres()
-assert(env, err)
+env = assert (luasql.postgres())
 -- connect to data source
-con, err = env:connect("luasql-test")
-assert(con, err)
+con = assert (env:connect("luasql-test"))
 -- reset our table
-res, err = con:execute"DROP TABLE people"
-res, err = con:execute[[
+res = con:execute"DROP TABLE people"
+res = assert (con:execute[[
   CREATE TABLE people(
     name  varchar(50),
     email varchar(50)
   )
-]]
-assert(res, err)
+]])
 -- add a few elements
 list = {
-    { name="Tomas Guisasola", email="tomas@kepler.org", },
-    { name="Roberto Ierusalimschy", email="roberto@kepler.org", },
-    { name="Andre Carregal", email="carregal@kepler.org", },
+  { name="Jose das Couves", email="jose@couves.com", },
+  { name="Manoel Joaquim", email="manoel.joaquim@cafundo.com", },
+  { name="Maria das Dores", email="maria@dores.com", },
 }
 for i, p in pairs (list) do
-  res, err = con:execute(string.format([[
+  res = assert (con:execute(string.format([[
     INSERT INTO people
     VALUES ('%s', '%s')]], p.name, p.email)
-  )
-  assert(res, err)
+  ))
 end
 -- retrieve a cursor
-cur, err = con:execute"SELECT name, email from people"
-assert(cur, err)
+cur = assert (con:execute"SELECT name, email from people")
 -- print all rows
 row = cur:fetch ({}, "a")	-- the rows will be indexed by field names
 while row do
-	print(string.format("Name: %s, E-mail: %s", row.name, row.email))
-	row = cur:fetch (row, "a")	-- reusing the table of results
+  print(string.format("Name: %s, E-mail: %s", row.name, row.email))
+  row = cur:fetch (row, "a")	-- reusing the table of results
 end
 -- close everything
 cur:close()
