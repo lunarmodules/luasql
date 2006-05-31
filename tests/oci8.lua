f2c6fcb960b32cfa7a8cbf4b88cb7e711ef57cfc
carregal <carregal>
2006-05-31 21:43:15 +0000

    Correct the tests to run on Oracle OCI8

--- tests/oci8.lua
+++ tests/oci8.lua
@@ -1,7 +1,10 @@
 ---------------------------------------------------------------------
 -- Oracle specific tests and configurations.
--- $Id: oci8.lua,v 1.2 2006/01/25 19:15:21 tomas Exp $
+-- $Id: oci8.lua,v 1.3 2006/05/31 21:43:15 carregal Exp $
 ---------------------------------------------------------------------
 
 table.insert (CUR_METHODS, "numrows")
 table.insert (EXTENSIONS, numrows)
+
+DEFINITION_STRING_TYPE_NAME = "varchar(60)"
+QUERYING_STRING_TYPE_NAME = "string"
