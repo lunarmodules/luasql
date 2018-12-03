acf8e021bf39dd2934df25f6b36fe4c68ffb0383
Tomás Guisasola <tomasguisasola@gmail.com>
2018-12-03 16:25:04 -0200

    Adding cur:seek() method to MySQL driver.

--- tests/mysql.lua
+++ tests/mysql.lua
@@ -1,12 +1,34 @@
 ---------------------------------------------------------------------
 -- MySQL specific tests and configurations.
--- $Id: mysql.lua,v 1.4 2006/01/25 20:28:30 tomas Exp $
 ---------------------------------------------------------------------
 
 QUERYING_STRING_TYPE_NAME = "binary(65535)"
 
+---------------------------------------------------------------------
+-- Seeks to an arbitrary row in a query result set.
+---------------------------------------------------------------------
+function seek ()
+    -- Inserts three rows.
+    assert2 (3, CONN:execute"insert into t (f1) values ('a'), ('b'), ('c')", "could not insert a new record")
+    cur = CUR_OK(CONN:execute"select * from t")
+    assert2('a', cur:fetch())
+    assert2('b', cur:fetch())
+	cur:seek(1)
+    assert2('b', cur:fetch())
+    assert2('c', cur:fetch())
+	cur:seek(1)
+    assert2('b', cur:fetch())
+    assert2('c', cur:fetch())
+    assert2(nil, cur:fetch())
+    cur:close()
+
+	io.write (" seek")
+end
+
 table.insert (CUR_METHODS, "numrows")
 table.insert (EXTENSIONS, numrows)
+table.insert (CUR_METHODS, "seek")
+table.insert (EXTENSIONS, seek)
 table.insert (CONN_METHODS, "escape")
 table.insert (EXTENSIONS, escape)
 
