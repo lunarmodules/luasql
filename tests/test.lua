32bec250f48c01e79fe17130a03db8fa19d7eab2
Geraldo Luiz <geraldoluiz@puc-rio.br>
2025-11-15 13:43:12 -0300

    cleaned up some debugging print leftovers

--- tests/test.lua
+++ tests/test.lua
@@ -148,7 +148,6 @@ function basic_test ()
 	checkUnknownDatabase(ENV)
 
 	-- force garbage collection
-	-- This sometimes triggers a segmentation fault with duckdb
 	local a = {}
 	setmetatable(a, {__mode="v"})
 	a.ENV = ENV_OK (luasql[driver] ())
@@ -188,15 +187,12 @@ end
 ---------------------------------------------------------------------
 function fetch2 ()
 	-- check number of lines
-	print"a"
 	local cur0 = CUR_OK (CONN:execute ("select count(*) from t"))
 	assert2 (0, tonumber (cur0:fetch()))
 	assert (cur0:close(), "couldn't close the cursor after counting rows from t")
-	print"b"
 
 	assert (CONN:close(), "couldn't close the connection after creating a table!")
 	CONN = CONN_OK (ENV:connect (datasource, username, password))
-	print"c"
 
 	-- insert a record.
 	assert2 (1, CONN:execute ("insert into t (f1, f2) values ('b', 'c')"))
@@ -488,7 +484,6 @@ function rollback ()
 	assert2 (true, cur:close(), "couldn't close cursor")
 	assert2 (false, cur:close())
 	assert2 (true, CONN:commit(), "couldn't commit transaction")
-	print"here"
 	-- insert a record and roll back the operation.
 	assert2 (1, CONN:execute ("insert into t (f1) values ('b')"))
 	local cur = CUR_OK (CONN:execute ("select count(*) from t"))
