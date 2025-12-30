359fda6751c95e7dec9bb05cbdaaf061bb2a78b7
Tomás Guisasola <tomas@puc-rio.br>
2025-12-30 13:46:37 -0300

    Improvement in tests of to-be-closed variables

--- tests/to_be_closed_support.lua
+++ tests/to_be_closed_support.lua
@@ -4,11 +4,25 @@
 
 assert(CONN, "Unable to access CONN variable with a connection object!")
 
-local cursor <close> = CONN:execute("select * from t")
-CUR_OK (cursor)
+local t = {}
 
-local connection <close> = ENV:connect (datasource, username, password)
-CONN_OK (connection)
+do
+	local cursor <close> = CUR_OK (CONN:execute("select count(*) from t"))
+	t.cursor = cursor
+	assert (tostring(t.cursor):match"cursor %(0x%w+%)", "cursor was closed")
+end
+assert (tostring(t.cursor):match"cursor %(closed%)", "cursor already open")
 
-local environment <close> = luasql[driver] ()
-ENV_OK (environment)
+do
+	local connection <close> = CONN_OK (ENV:connect (datasource, username, password))
+	t.connection = connection
+	assert (tostring(t.connection):match"connection %(0x%w+%)", "connection was closed")
+end
+assert (tostring(t.connection):match"connection %(closed%)", "connection already open")
+
+do
+	local environment <close> = ENV_OK (luasql[driver] ())
+	t.environment = environment
+	assert (tostring(t.environment):match"environment %(0x%w+%)", "environment was closed")
+end
+assert (tostring(t.environment):match"environment %(closed%)", "environment already open")
